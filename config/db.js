55ba19766c2dfdff8d35fe0b011903d6d9e1bddb
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-05 16:31:52 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -16,9 +16,9 @@ async function connectDB() {
       bufferCommands: false,
     };
 
-    // console.log("⏳ Connecting to MongoDB...");
+    console.log("⏳ Connecting to MongoDB...");
     cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/nextbuy`, opts).then((mongoose) => {
-      // console.log("✅ MongoDB connected successfully.");
+      console.log("✅ MongoDB connected successfully.");
       return mongoose;
     })
 
