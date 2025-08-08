cce97061b6c9e1716b227de5744edf88fc154245
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-08 22:38:20 +0530

    Enable CORS and JSON parsing in Express app
    Added middleware to support JSON request bodies and enabled CORS for requests from http://localhost:5173. Also removed the unused test script from package.json.

--- api/index.js
+++ api/index.js
@@ -8,6 +8,13 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
