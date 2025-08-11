127e6812d2de3c4540ac65fda853f7251da43a54
Sahil Zalte <zaltesahil94@gmail.com>
2025-08-11 23:32:38 +0530

    Implement show, update, and delete task endpoints
    Added implementations for showTask, updateTask, and deleteTask controller functions. Updated error handling to return error messages from exceptions. Improved response consistency and removed unused placeholder functions.

--- api/controllers/Task.controller.js
+++ api/controllers/Task.controller.js
@@ -17,25 +17,83 @@ export const createTask = async (req, res) => {
     } catch (error) {
         res.status(500).json({
             status: false,
-            message: "Failed to create task",
+            message: error.message
         });
     }
 }
+
 export const getAllTask = async (req, res) => {
     try {
         const taskData = await TaskModel.find().sort({ createdAt: -1 }).lean().exec()
         res.status(200).json({
             status: true,
-            message: "Tasks retrieved successfully",
             taskData
         });
     } catch (error) {
         res.status(500).json({
             status: false,
-            message: "Failed retrieved task",
+            message: error.message
         });
     }
 }
-export const showTask = async (req, res) => { }
-export const updateTask = async (req, res) => { }
-export const deleteTask = async (req, res) => { }
+
+export const showTask = async (req, res) => {
+    try {
+
+        const { taskid } = req.params;
+
+        const taskData = await TaskModel.findById(taskid).lean().exec()
+        res.status(200).json({
+            status: true,
+            taskData
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+}
+
+export const updateTask = async (req, res) => {
+    try {
+        const { taskid } = req.params;
+        const { title, description, status } = req.body;
+
+        const taskData = await TaskModel.findByIdAndUpdate(taskid, { title, description, status }, { new: true })
+        if (!taskData) {
+            return res.status(404).json({
+                status: false,
+                message: "Task not found"
+            });
+        }
+        res.status(200).json({
+            status: true,
+            message: "Task updated successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    try {
+        const { taskid } = req.params;
+
+        await TaskModel.findByIdAndDelete(taskid)
+
+        res.status(200).json({
+            status: true,
+            message: "Task deleted successfully",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: error.message
+        });
+    }
+}
